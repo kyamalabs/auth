e0777b8148b1cecbe4670aa165e6468a044ed8b4
m16w <m16w@kyamagames.com>
2024-01-03 15:49:20 -0800

    add authenticate account handler tests

--- internal/api/handler/rpc_authenticate_account_test.go
+++ internal/api/handler/rpc_authenticate_account_test.go
@@ -2,10 +2,14 @@ package handler
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"testing"
 	"time"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/status"
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/google/uuid"
@@ -104,6 +108,118 @@ func TestAuthenticateAccountAPI(t *testing.T) {
 
 				require.Equal(t, authenticateAccountReqParams.WalletAddress, res.GetAccount().GetOwner())
 				require.Equal(t, "bearer", strings.ToLower(res.GetSession().GetTokenType()))
+			},
+		},
+		{
+			name: "Failure - invalid request arguments",
+			req: &pb.AuthenticateAccountRequest{
+				WalletAddress: "0x0000000000000000000000000000000000000000",
+				Challenge:     authenticateAccountReqParams.GetChallenge(),
+				Signature:     "invalid_signature",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+			},
+			checkResponse: func(t *testing.T, res *pb.AuthenticateAccountResponse, err error) {
+				require.Error(t, err)
+				require.Empty(t, res)
+
+				var violations []string
+				expectedFieldViolations := []string{"wallet_address", "signature"}
+
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				details := st.Details()
+				for _, detail := range details {
+					br, ok := detail.(*errdetails.BadRequest)
+					require.True(t, ok)
+					fieldViolations := br.FieldViolations
+					for _, violation := range fieldViolations {
+						violations = append(violations, violation.Field)
+					}
+				}
+
+				require.ElementsMatch(t, expectedFieldViolations, violations)
+			},
+		},
+		{
+			name: "Failure - invalid signature",
+			req: &pb.AuthenticateAccountRequest{
+				WalletAddress: authenticateAccountReqParams.GetWalletAddress(),
+				Challenge:     authenticateAccountReqParams.GetChallenge(),
+				Signature:     fmt.Sprintf("%sz", authenticateAccountReqParams.GetSignature()[:len(authenticateAccountReqParams.GetSignature())-1]),
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+			},
+			checkResponse: func(t *testing.T, res *pb.AuthenticateAccountResponse, err error) {
+				require.Empty(t, res)
+				require.Error(t, err)
+				require.ErrorContains(t, err, SignatureVerificationError)
+			},
+		},
+		{
+			name: "Failure - could not get account by owner",
+			req:  authenticateAccountReqParams,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountByOwner(gomock.Any(), authenticateAccountReqParams.WalletAddress).
+					Times(1).
+					Return(db.Account{}, errors.New("some db error"))
+			},
+			checkResponse: func(t *testing.T, res *pb.AuthenticateAccountResponse, err error) {
+				require.Empty(t, res)
+				require.Error(t, err)
+				require.ErrorContains(t, err, InternalServerError)
+			},
+		},
+		{
+			name: "Failure - could not create new account",
+			req:  authenticateAccountReqParams,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountByOwner(gomock.Any(), authenticateAccountReqParams.WalletAddress).
+					Times(1).
+					Return(db.Account{}, db.RecordNotFoundError)
+
+				store.EXPECT().
+					CreateAccount(gomock.Any(), authenticateAccountReqParams.WalletAddress).
+					Times(1).
+					Return(db.Account{}, errors.New("some db error"))
+			},
+			checkResponse: func(t *testing.T, res *pb.AuthenticateAccountResponse, err error) {
+				require.Empty(t, res)
+				require.Error(t, err)
+				require.ErrorContains(t, err, InternalServerError)
+			},
+		},
+		{
+			name: "Failure - could not create new db session",
+			req:  authenticateAccountReqParams,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountByOwner(gomock.Any(), authenticateAccountReqParams.WalletAddress).
+					Times(1).
+					Return(db.Account{}, db.RecordNotFoundError)
+
+				store.EXPECT().
+					CreateAccount(gomock.Any(), authenticateAccountReqParams.WalletAddress).
+					Times(1).
+					Return(db.Account{
+						ID:        uuid.New(),
+						Owner:     authenticateAccountReqParams.WalletAddress,
+						Role:      db.Role(token.Gamer),
+						CreatedAt: time.Now().UTC(),
+					}, nil)
+
+				store.EXPECT().
+					CreateSession(gomock.Any(), EqCreateSessionParams(db.CreateSessionParams{
+						WalletAddress: authenticateAccountReqParams.WalletAddress,
+					})).
+					Return(db.Session{}, errors.New("some db error"))
+			},
+			checkResponse: func(t *testing.T, res *pb.AuthenticateAccountResponse, err error) {
+				require.Empty(t, res)
+				require.Error(t, err)
+				require.ErrorContains(t, err, InternalServerError)
 			},
 		},
 	}
